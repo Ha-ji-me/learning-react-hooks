4d999ea453b604642e7be9bffe744b1d07d49a50
Ha-ji-me <hjmx3599@gmail.com>
2023-10-15 17:53:58 +0900

    4-6/メモアプリ: memoコンポーネント作成

--- src/memo/Memo.jsx
+++ src/memo/Memo.jsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import usePersist from "../Persist";
-//Itemも読み込むこと
+import Item from "./Item";
 
-function Memo() {
+function Memo(props) {
+  const [memo, setMemo] = usePersist("memo", []);
+  const [findMemo, setFmemo] = usePersist("findMemo" ,[]);
+  const [mode, setMode] = usePersist('mode', 'default');
+
+  let data = [];
+
+  switch (mode) {
+    case 'false':
+      data = memo.map((value, key) => (
+        <Item key={value.message} value={value} index={key + 1} />
+      ))
+      setMode('default')
+      break
+
+    case 'find':
+      data = findMemo.map((value, key) => (
+        <Item key={value.message} value={value} index={key + 1} />
+      ))
+      break
+
+    default:
+      data = memo.map((value, key) => (
+        <Item key={value.message} value={value} index={key + 1} />
+      ))
+  }
+
   return(
     <table className="table mt-4">
-      <tbody>itemが入ります</tbody>
+      <tbody>{data}</tbody>
     </table>
   )
 }
+
+export default Memo;
